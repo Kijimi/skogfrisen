350906d44e4baa89ddd62ff41b5587447a4ba94e
Kijimi <30993332+Kijimi@users.noreply.github.com>
2017-10-29 14:25:12 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,10 +16,14 @@ bot.on("ready", async ready => {
 
 bot.on("message", async message => {
 	if(message.author.bot) return;
-
 	if(message.content.indexOf(botSettings.prefix) !== 0) return;
-	const args = message.content.slice(botSettings.prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
+
+	let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(botSettings.prefix)) return;
+
 	if(command === "ping") {
 		const m = await message.channel.send("Calculating ping...");
 		m.edit(`Skogfrisen's latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms.`);
@@ -31,7 +35,7 @@ bot.on("message", async message => {
 	if(command === "nsfw") {
 		const member = message.member;
 		const role = message.member.guild.roles.find('name', 'no-nsfw');
-		
+
 		if (member.roles.find('name', 'no-nsfw')) {	//	Has role, so remove it
 			member.removeRole(role).then(m => {
 				console.log(`Removed role from ${m.nickname}`);
@@ -42,40 +46,37 @@ bot.on("message", async message => {
 			}).catch(console.error);
 		}
 	}
-});
 
 
-bot.on("message", async message => {
-    if(message.author.bot) return; 
+	if(command === `wiki`) {
+		const { RichEmbed } = require('discord.js');
+		const entries = {
+			soren: {
+				title: "Soren",
+				color: "#5dade2",
+				description: "Soren (pronounced sorr-en) is a male Barn Owl, or Tyto alba, and was the main protagonist of the first six Guardians of Ga'Hoole books and the deuteragonist of the last 9 books, as well as the leader of the Band and creator of the Chaw of Chaws.",
+				image: "https://vignette.wikia.nocookie.net/guardiansofgahoole/images/d/dd/Soren9937.JPG/revision/latest?cb=20161225012250",
+				url: "URL HERE"
+			}
+		};
 
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
+		let entrySearch = args[0].toLowerCase();
+		let entry = entries[entrySearch];
 
-    if(!command.startsWith(botSettings.prefix)) return;
+		let embed = new RichEmbed()
+						.setTitle(entry.title)
+						.setURL(entry.url)
+						.setThumbnail(entry.image)
+						.setDescription(entry.description)
+						.setColor(entry.color);
 
-    if(command === `${botSettings.prefix}soren`) {
-	const { RichEmbed } = require('discord.js');
-	const entries = {
-		soren: {
-			title: "Soren",
-			color: "#5dade2",
-			description: "Soren (pronounced sorr-en) is a male Barn Owl, or Tyto alba, and was the main protagonist of the first six Guardians of Ga'Hoole books and the deuteragonist of the last 9 books, as well as the leader of the Band and creator of the Chaw of Chaws.",
-			image: "https://vignette.wikia.nocookie.net/guardiansofgahoole/images/d/dd/Soren9937.JPG/revision/latest?cb=20161225012250",
-			url: "URL HERE"
-		}
-	};
+		return message.channel.send({ embed });
+	}
 
-	let entrySearch = args[0].toLowerCase();
-	let entry = entries[entrySearch];
-	
-	let embed = new RichEmbed()
-					.setTitle(entry.title)
-					.setURL(entry.url)
-					.setThumbnail(entry.image)
-					.setDescription(entry.description)
-					.setColor(entry.color);
 
-	return message.channel.send({ embed });
-}
+
+
+});	//	DO NOT TOUCH THIS
+
+
 bot.login(botSettings.token);
